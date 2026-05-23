0149b58ea12146f32ea207b82c211ffdcb0c8d2d
pushpender-79 <pushpenderchauhan79@gmail.com>
2026-05-23 12:55:32 +0530

    understanding some concept of component based css and tailwind css

---
+++ 06-Tailwind-Css/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-gray-800 text-red-300 flex items-center justify-center '>
+  Hey Bro this is Tailwind CSS intro by SHERIYANS Coding School!!!
+    </div>
+  )
+}
+
+export default App
