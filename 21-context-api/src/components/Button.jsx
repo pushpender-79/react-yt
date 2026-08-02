48e4e7ef1bc7d14d146689a90365f245f1f21ee3
pushpender-79 <pushpenderchauhan79@gmail.com>
2026-08-02 20:15:58 +0530

    implemented context api...through a simple navbar

---
+++ 21-context-api/src/components/Button.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react'
+import { ThemeDataContext } from '../context/ThemeContext'
+
+const Button = () => {
+const [theme,setTheme]=useContext(ThemeDataContext)
+const changeTheme = ()=>{
+setTheme("Dark")
+}
+  return (
+    <div>
+      <button onClick={changeTheme}>Change Theme</button>
+    </div>
+  )
+}
+
+export default Button
